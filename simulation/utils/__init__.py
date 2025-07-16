4a3c3ae218287d0861a84d8bf6d88fdd2cada601
TheHuntsman4 <anikethvij464@gmail.com>
2025-07-15 23:35:29 -0700

    feat: adds a custom digestion function to make the converation memory more efficient.

--- simulation/utils/__init__.py
+++ simulation/utils/__init__.py
@@ -1,4 +1,4 @@
 from .agent_base import AgentBase
-from .log_memory import log_conversation
+from .log_memory import log_conversation, digest_conversation, generate_conversation_id
 
 __all__ = ["AgentBase", "log_conversation"]
